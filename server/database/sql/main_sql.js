5b5a8867c01d7f55415289f05165a568dbd85600
taehotaeho <jackt0506@naver.com>
2026-03-23 14:42:06 +0900

    이태호/우선순위변환-메인페이지/20260323

--- server/database/sql/main_sql.js
+++ server/database/sql/main_sql.js
@@ -1,9 +1,10 @@
 // 메인페이지 관련 sql문
 const selectById = `
 SELECT gu.name as 보호자, 
-       iu.name as 담당자,
-       su.name as 지원대상자,
-       sv.created_at as 지원신청일
+      iu.name as 담당자,
+      su.name as 지원대상자,
+      sv.created_at as 지원신청일,
+      sv.result
 FROM GeneralUser_Tbl gu
 INNER JOIN InstiUser_Tbl iu
 ON gu.institution_id = iu.institution_id
--- server/services/main_service.js
+++ server/services/main_service.js
@@ -4,6 +4,24 @@ const mainMapper = require("../database/mappers/main_mapper.js");
 // 메인페이지 조사지 내역 조회
 const findById = async (id) => {
   let list = await mainMapper.selectById(id);
+  console.log(list.result);
+  let rank = list.result;
+  switch (rank) {
+    case "f003":
+      rank = "긴급";
+      break;
+    case "f002":
+      rank = "중점";
+      break;
+    case "f001":
+      rank = "계획";
+      break;
+
+    default:
+      rank = "심사중";
+      break;
+  }
+  list.result = rank;
   return list;
 };
 
