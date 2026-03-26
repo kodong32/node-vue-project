d495291f15fa66a09c9c149f92f4177c1a06930d
taehotaeho <jackt0506@naver.com>
2026-03-26 11:47:36 +0900

    이태호/우선순위요청 라우터-우선순위요청/20260326

---
+++ client/src/router/priority_router.js
@@ -0,0 +1,9 @@
+// priority_router.js 우선순위 요청
+
+export default [
+  {
+    path: "/manager/priority-request/:id",
+    name: "PriorityRequest",
+    component: () => import("../views/manager/PriorityRequest.vue"),
+  },
+];
